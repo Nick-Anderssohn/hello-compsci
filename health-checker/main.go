79335521638dc1bc640a0df437bb685287f74c95
Nick Anderssohn <ncadevelopment@gmail.com>
2017-07-02 22:24:57 -0700

    revert back to old method of checking health

--- health-checker/main.go
+++ health-checker/main.go
@@ -2,53 +2,31 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os/exec"
 	"time"
 )
 
-const targetURL = "http://0.0.0.0:8079/"
-
-var testCode = `#include <stdio.h>
-int main() {
-printf("test");
-}`
-
-type codeReader struct{}
-
-func (c codeReader) Read(p []byte) (int, error) {
-	for i, char := range testCode {
-		p[i] = byte(char)
-	}
-	return len(testCode), nil
-}
+const targetURL = "http://0.0.0.0:8079/healthcheck"
 
 func main() {
 	fmt.Println("starting health checker")
-	for range time.Tick(time.Second) {
-		// every second send a health check to code-runner
-		var client http.Client
-		req, _ := http.NewRequest("POST", targetURL, bytes.NewBufferString(testCode))
-		req.Header.Add("Filename", "test.c")
+	var client http.Client
+	req, _ := http.NewRequest("GET", targetURL, nil)
+	for range time.Tick(time.Second * 30) {
+		// every 30 seconds send a health check to code-runner
 		checkHealth(client, req)
 	}
 }
 
 func checkHealth(client http.Client, req *http.Request) {
 	resp, err := client.Do(req)
-	// fmt.Println(err.Error())
 	if err != nil {
 		fmt.Println("Health check failed! Creating new code-runner")
 		createNewCodeRunner()
 	} else {
 		defer resp.Body.Close() // only close if there was not an error
-		if body, _ := ioutil.ReadAll(resp.Body); string(body) != "test" {
-			fmt.Println("Health check failed! Creating new code-runner")
-			createNewCodeRunner()
-		}
 	}
 }
 
--- health-checker/restart_coderunner.sh
+++ health-checker/restart_coderunner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker kill code-runner
+docker stop code-runner
 docker rm code-runner
 docker run --net helloclass_hello-class-network -ditp 8079:8079 --name code-runner code-runner
